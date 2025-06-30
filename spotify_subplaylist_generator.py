4c273fc70013510577cdba13fb477732977f1c20
Eric <62779413+TheBigSwig@users.noreply.github.com>
2025-06-30 00:26:15 -0700

    Add example code for retrieving most recent saved songs

--- spotify_subplaylist_generator.py
+++ spotify_subplaylist_generator.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
 
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
 def main():
-    pass
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope="user-library-read"))
+    
+    results = sp.current_user_saved_tracks()
+    for idx, item in enumerate(results['items']):
+        track = item['track']
+        print(idx, track['artists'][0]['name'], " – ", track['name'])
 
 if __name__ == "__main__":
     main()
