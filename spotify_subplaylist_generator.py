1bb9eeacab409bb936518c8679585f310cf5afe6
Eric <62779413+TheBigSwig@users.noreply.github.com>
2025-07-02 00:04:27 -0700

    Add helper function for retrieving user playlists

--- spotify_subplaylist_generator.py
+++ spotify_subplaylist_generator.py
@@ -3,13 +3,37 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
+from typing import Dict, List
+
+def get_user_playlists(spotify_client : spotipy.Spotify) -> List[Dict]:
+    user_playlists = []
+    num_playlists_per_request = 50
+
+    api_response = spotify_client.current_user_playlists(limit=num_playlists_per_request)
+    while len(api_response['items']) > 0:
+        for playlist in api_response['items']:
+            user_playlists.append(playlist)
+        api_response = spotify_client.current_user_playlists(limit=num_playlists_per_request,
+                                                             offset=len(user_playlists))
+    return user_playlists
+
 def main():
-    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope="user-library-read"))
+    print("Welcome to the Spotify Subplaylist Generator!")
     
-    results = sp.current_user_saved_tracks()
-    for idx, item in enumerate(results['items']):
-        track = item['track']
-        print(idx, track['artists'][0]['name'], " – ", track['name'])
+    print("\nRetrieving Spotify access token...")
+    spotify_client = spotipy.Spotify(auth_manager=SpotifyOAuth(scope="playlist-modify-public"))
+
+    print("\nRetrieving user playlist information...")
+    user_playlists = get_user_playlists(spotify_client)
+    
+    if len(user_playlists) == 0:
+        print("User does not have any public playlists, so subplaylists cannot be generated")
+        print("Exiting...")
+        return
+    
+    print("\nUser playlists:")
+    for playlist in user_playlists:
+        print(f"> {playlist['name']}")
 
 if __name__ == "__main__":
     main()
