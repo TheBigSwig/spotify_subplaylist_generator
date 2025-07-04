0d9cef1ce59553db15db07afdae49a1539eae9c2
Eric <62779413+TheBigSwig@users.noreply.github.com>
2025-07-04 02:11:01 -0700

    Add helper function for creating playlist from a list of tracks

--- spotify_subplaylist_generator.py
+++ spotify_subplaylist_generator.py
@@ -52,6 +52,19 @@ def get_artist_info_for_tracks(spotify_client : spotipy.Spotify, tracks : List[D
             all_artists_info[artist_info['uri']] = artist_info
     return all_artists_info
 
+def create_playlist_with_tracks(spotify_client : spotipy.Spotify, playlist_name : str, tracks : List[Dict]) -> None:
+    new_playlist_info = spotify_client.user_playlist_create(spotify_client.current_user()['id'], playlist_name)
+
+    batch_size = 100
+    for slice_index in range(math.ceil(len(tracks) / batch_size)):
+        track_batch = tracks[(slice_index * batch_size) : ((slice_index + 1) * batch_size)]
+        
+        track_batch_uris = []
+        for track in track_batch:
+            track_batch_uris.append(track['track']['uri'])
+        
+        spotify_client.playlist_add_items(new_playlist_info['id'], track_batch_uris)
+
 def main():
     print("Welcome to the Spotify Subplaylist Generator!")
     
